626620ba8adee2167cfa9dc785cef0df07e90a51
NorthisRP <philw7321980@gmail.com>
2022-02-21 01:14:37 +0300

    filtering done

--- src/pages/Cats/index.jsx
+++ src/pages/Cats/index.jsx
@@ -1,20 +1,46 @@
-import { Grid } from "@mui/material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { Box, Grid, Tab } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import catsService from "./../../services/cats.service";
 import CatCard from "./components/CatCard";
 
 export default function Cats() {
   const [cats, setCats] = useState([]);
+  const [tab, setTab] = useState("all");
 
   useEffect(() => {
-    catsService.getAllCats(1, 20).then((res) => setCats(res.data.items));
-  }, []);
+    switch (tab) {
+      case "all":
+        return catsService
+          .getAllCats(1, 20)
+          .then((res) => setCats(res.data.items));
+      case "booked":
+        return catsService.getBookedCats().then((res) => setCats(res.data));
+      case "unbooked":
+        return catsService.getNotBookedCats().then((res) => setCats(res.data));
+      default:
+        return;
+    }
+  }, [tab]);
+
+  const handleTab = (event, value) => {
+    setTab(value);
+  };
 
   return (
-    <Grid container spacing={2}>
-      {cats.map((cat) => (
-        <CatCard cat={cat} />
-      ))}
-    </Grid>
+    <TabContext value={tab}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <TabList onChange={handleTab}>
+          <Tab label="All cats" value="all" />
+          <Tab label="Booked cats" value="booked" />
+          <Tab label="Unbooked cats" value="unbooked" />
+        </TabList>
+      </Box>
+      <Grid container spacing={2} style={{ padding: 24 }}>
+        {cats.map((cat) => (
+          <CatCard cat={cat} key={cat.id} />
+        ))}
+      </Grid>
+    </TabContext>
   );
 }
